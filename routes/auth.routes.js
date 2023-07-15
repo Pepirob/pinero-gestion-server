3c73169bc041ef8229c56605852699db78550e96
Jose Luis <pepirob>
2023-07-15 17:58:03 +0200

    add authentication routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -22,3 +22,56 @@ router.post('/signup', async (req, res, next) => {
         next(error)
     }
 })
+
+router.post("/login", async (req, res, next) => {
+    const { identifier, password } = req.body;
+
+    if (!identifier || !password) {
+        res.status(400).json({ errorMessage: "Los campos deben estar llenos" });
+        return;
+    }
+
+    try {
+        const foundUser = await User.findOne({
+            $or: [{ username: identifier }, { email: identifier }],
+        });
+        const errorMessage = "Email o contraseña no válidos";
+
+        if (!foundUser) {
+            res.status(400).json({ errorMessage });
+            return;
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            foundUser.password
+        );
+
+        if (!isPasswordCorrect) {
+            res.status(400).json({ errorMessage });
+            return;
+        }
+
+        const payload = {
+            _id: foundUser._id,
+            email: foundUser.email,
+        };
+
+        const tokenConfig = {
+            algorithm: "HS256",
+            expiresIn: "3d",
+        };
+
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, tokenConfig);
+
+        res.status(200).json({ authToken });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/verify", isAuthenticated, (req, res) => {
+    res.status(200).json(req.payload);
+});
+
+module.exports = router
